b7f09013851114adff2c5ee614644a253bb51eca
fernandomiras1 <ernando.miras.pc@gmail.com>
2020-05-03 00:28:11 -0300

    register api firebase v3

--- src/service-worker.js
+++ src/service-worker.js
@@ -102,7 +102,7 @@ workbox.routing.registerRoute(
 // https://www.googleapis.com/identitytoolkit/v3/relyingparty
 workbox.routing.registerRoute(
     new RegExp('^https:\/\/www\.googleapis\.com.*'),
-    workbox.strategies.NetworkFirst({
+    workbox.strategies.networkFirst({
       cacheName: `auth-firebase-app`,
       plugins: [
         new workbox.expiration.Plugin({
