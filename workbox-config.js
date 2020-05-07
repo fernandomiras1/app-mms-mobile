54534a08976727231dbdf6afc8063ae63ff9f907
fernandomiras1 <ernando.miras.pc@gmail.com>
2020-05-07 10:40:46 -0300

    workbox-config srr

--- workbox-config.js
+++ workbox-config.js
@@ -3,13 +3,13 @@ const workboxBuild = require('workbox-build');
 const buildSW = () => {
   // This will return a Promise
   return workboxBuild.injectManifest({
-    globDirectory: "dist/app-mms-mobile",
+    globDirectory: "dist/app-mms-mobile/browser",
     globPatterns: [
       '**\/*.{js,css,html}',
       "assets/**/*.{css,html,js,json,png,svg,woff,woff2}"
     ],
     swSrc: "src/service-worker.js",
-    swDest: "dist/app-mms-mobile/service-worker.js"
+    swDest: "dist/app-mms-mobile/browser/service-worker.js"
   }).then(({count, size, warnings}) => {
     // Optionally, log any warnings and details.
     warnings.forEach(console.warn);
